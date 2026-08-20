93cae34bd10512019affe1aec657f08d2346cf6d
조수인 <csicho92@gmail.com>
2026-08-20 18:47:56 +0900

    첫 커밋

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import Header  from '../components/Header' ;
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>안녕하세요, 조수인 입니다. </h1>
+    </div>
+  )
+}
+
+export default App
